022750badc862af6352f7a7847a34819601ba1f3
Ravina Prof <ravinabhosale5555@gmail.com>
2023-01-05 14:38:26 +0530

    Trying object class

---
+++ Day7/CollectionSolution/Tester/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+List<Object> allObjects = new List<Object>();
+allObjects.Add(12);
+allObjects.Add(true);
+allObjects.Add(new { id=15, title="abc"});
+allObjects.Add(new { id=2, title="def"});
+allObjects.Add(new { id=12, title="hij"});
+allObjects.Add(new { id=6, title="klm"});
+allObjects.Add(new { id=9, title="nopsds"});
+
+foreach(object o in allObjects){
+    Console.WriteLine(o);
+}
