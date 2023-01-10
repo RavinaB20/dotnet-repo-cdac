8554a1015bb6374396e88a26ad96be911732306b
Ravina Prof <ravinabhosale5555@gmail.com>
2023-01-10 18:22:26 +0530

    Database connectivity from product table

--- Day10_Ecommerce/Ecommerce/BLL/CatalogManager.cs
+++ Day10_Ecommerce/Ecommerce/BLL/CatalogManager.cs
@@ -6,16 +6,21 @@ public class CatalogManager
 {
     public List<Product> GetAllProducts(){
         List<Product> allProducts = new List<Product>();
-        Console.WriteLine("***************");
-     //   allProducts =  DBManager.GetAllProducts();
-        allProducts = DBManager.GetAllProductsFromFile("../DAL/Products.json");
-        Console.WriteLine("All products from file");
+        
+        /* TO GET HARDCODED DATA - USER DEFINED LIST
+          allProducts =  DBManager.GetAllProducts();
+        */
+        /*  TO GET PRODUCTS FROM FILE
+            allProducts = DBManager.GetAllProductsFromFile("../DAL/Products.json");
+        */
+        //GET PRODUCT FROM DB
+        allProducts = DBManager.GetAllProductsFromDatabase();
         return allProducts;
     }
 
     public Product GetProduct(int id){
         List<Product> allProducts = new List<Product>();
-        allProducts = DBManager.GetAllProducts();
+        allProducts = DBManager.GetAllProductsFromDatabase();
         Product foundProduct = null;
         foreach(Product prod in  allProducts){
             if(prod.ProductId == id){
--- Day10_Ecommerce/Ecommerce/DAL/DBManager.cs
+++ Day10_Ecommerce/Ecommerce/DAL/DBManager.cs
@@ -1,9 +1,45 @@
 ﻿namespace DAL;
 using BOL;
 using System.Text.Json;
+using MySql.Data.MySqlClient;
 
 public class DBManager
 {
+    public static string conStr = @"server=localhost;port=3306;user=root;password=root123;database=ravinaorg";
+
+    public static List<Product> GetAllProductsFromDatabase(){
+        //Connected Data Access
+        List<Product> allProducts = new List<Product>();
+        MySqlConnection con = new MySqlConnection();
+        con.ConnectionString = conStr;
+        try{
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = con;
+            string query = "select * from products";
+            cmd.CommandText = query;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read()){
+                int productId = int.Parse(reader["productId"].ToString());
+                string productName = reader["productName"].ToString();
+                double price = double.Parse(reader["price"].ToString());
+                int quantity = int.Parse(reader["quantity"].ToString());
+
+                Product prod = new Product { ProductId = productId, ProductName=productName, Price = price, Quantity=quantity};
+                allProducts.Add(prod);
+            }
+        }   
+        catch(Exception e){
+            Console.WriteLine(e.Message);
+        } 
+        finally{
+            con.Close();
+        }
+        return allProducts;
+
+        return allProducts;
+    }
+
     public static List<Product> GetAllProducts(){
         List<Product> allProducts = new List<Product>();
         allProducts.Add(new Product{ProductId = 101, ProductName = "Pen" , Price = 10 , Quantity = 100});
@@ -14,10 +50,7 @@ public class DBManager
         return allProducts;
     }   
 
-    public static List<Product> GetAllProductsFromDatabase(){
-        List<Product> allProducts = new List<Product>();
-        return allProducts;
-    }
+
 
     public static List<Product> GetAllProductsFromFile(String path){
         string jsonString = File.ReadAllText(path);
